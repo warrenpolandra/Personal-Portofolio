39422573a813ee1a322efec7a2b1e69a92dbe13f
Warren Gerald Polandra <warren.polandra@gmail.com>
2024-09-04 23:12:30 +0800

    [main]: fix base url

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
